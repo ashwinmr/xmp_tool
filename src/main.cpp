7127a232dd0421417fd710d15a6b01797b557813
ashwinmr <iashwinrao@gmail.com>
2019-09-29 10:35:54 -0700

    Fix file path

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(int argc, const char *argv[])
 {
-	for (auto &str : GetTags("C:/Ashwin/My_Programming/Programs/xmp/temp/1.jpg"))
+	for (auto &str : GetTags("C:/Ashwin/My_Programming/Programs/xmp_tools/temp/1.jpg"))
 	{
 		cout << str << endl;
 	}
