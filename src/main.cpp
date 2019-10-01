44c6c05f0f4671cf50cdea72d65b370a53791a08
ashwinmr <iashwinrao@gmail.com>
2019-09-30 23:37:51 -0700

    Multi file and linting

--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,73 @@
+#include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
 #include <iostream>
 #include "xmp_tools.hpp"
-#include <boost/program_options.hpp>
 
 namespace po = boost::program_options;
+namespace fs = boost::filesystem;
 
 using namespace std;
 
 /**
  * Parse command line arguments
  */
-bool ParseArgs(int argc, const char **argv, po::variables_map &args)
-{
-	try
-	{
-		// Declare supported options
-		po::options_description desc("Allowed options:");
-		desc.add_options()("help,h", "Help message")
-		("path,p", po::value<std::string>()->required(), "Path to file or directory");
+bool ParseArgs(int argc, const char **argv, po::variables_map &args) {
+    try {
+        // Declare supported options
+        po::options_description desc("Allowed options:");
+        desc.add_options()("help,h", "Help message")("path,p", po::value<std::string>()->required(), "Path to file or directory");
 
-		// Make options positional
-		po::positional_options_description desc_p;
-		desc_p.add("path", 1);
+        // Make options positional
+        po::positional_options_description desc_p;
+        desc_p.add("path", 1);
 
-		// Store options
-		po::store(po::command_line_parser(argc, argv).options(desc).positional(desc_p).run(), args);
+        // Store options
+        po::store(po::command_line_parser(argc, argv).options(desc).positional(desc_p).run(), args);
 
-		// Handle help before checking for errors
-		if (args.count("help") || (argc < 2))
-		{
-			std::cout << desc << std::endl;
-			return false;
-		}
+        // Handle help before checking for errors
+        if (args.count("help") || (argc < 2)) {
+            std::cout << desc << std::endl;
+            return false;
+        }
 
-		// Check inputs for errors
-		po::notify(args);
+        // Check inputs for errors
+        po::notify(args);
 
-		// Parsing successful
-		return true;
-	}
-	catch (po::error &e)
-	{
-		std::cout << "Error: " << e.what() << std::endl;
-		return false;
-	}
+        // Parsing successful
+        return true;
+    } catch (po::error &e) {
+        std::cout << "Error: " << e.what() << std::endl;
+        return false;
+    }
 }
 
-int main(int argc, const char *argv[])
-{
-	// Parse args
-	po::variables_map args;
-	if (!ParseArgs(argc, argv, args))
-	{
-		return 0;
-	}
+int main(int argc, const char *argv[]) {
+    // Parse args
+    po::variables_map args;
+    if (!ParseArgs(argc, argv, args)) {
+        return 0;
+    }
 
-	for (auto &str : GetTags(args["path"].as<string>()))
-	{
-		cout << str << endl;
-	}
+    // Store path
+    string path = args["path"].as<string>();
 
-	return 0;
+    // Check if path is a file
+    if (!fs::is_directory(path)) {
+        for (auto &tag : GetTags(path)) {
+            cout << tag << endl;
+        }
+    } else {
+        for (fs::directory_entry &de : fs::directory_iterator(path)) {
+            vector<string> tags = GetTags(de.path().string());
+            if(tags.empty()){
+                continue;
+            }
+            for (auto &tag : tags) {
+                cout << tag << "\t";
+            }
+            cout << endl;
+        }
+    }
+
+    return 0;
 }
--- src/xmp_tools.cpp
+++ src/xmp_tools.cpp
@@ -1,6 +1,6 @@
-#include <vector>
-#include <string>
 #include <iostream>
+#include <string>
+#include <vector>
 
 // Must be defined to instantiate template classes
 #define TXMP_STRING_TYPE std::string
@@ -19,81 +19,69 @@ using namespace std;
 /**
  * Get tags from a file
  */
-vector<string> GetTags(string full_file_path)
-{
+vector<string> GetTags(string full_file_path) {
+    vector<string> tags;
 
-	vector<string> tags;
-
-	if (!SXMPMeta::Initialize())
-	{
-		cout << "Could not initialize toolkit!";
-		return tags;
-	}
-	XMP_OptionBits options = 0;
+    if (!SXMPMeta::Initialize()) {
+        cout << "Could not initialize toolkit!";
+        return tags;
+    }
+    XMP_OptionBits options = 0;
 #if UNIX_ENV
-	options |= kXMPFiles_ServerMode;
+    options |= kXMPFiles_ServerMode;
 #endif
-	// Must initialize SXMPFiles before we use it
-	if (!SXMPFiles::Initialize(options))
-	{
-		cout << "Could not initialize SXMPFiles.";
-		return tags;
-	}
+    // Must initialize SXMPFiles before we use it
+    if (!SXMPFiles::Initialize(options)) {
+        cout << "Could not initialize SXMPFiles.";
+        return tags;
+    }
 
-	try
-	{
-		// Options to open the file with - read only and use a file handler
-		XMP_OptionBits opts = kXMPFiles_OpenForRead | kXMPFiles_OpenUseSmartHandler;
+    try {
+        // Options to open the file with - read only and use a file handler
+        XMP_OptionBits opts = kXMPFiles_OpenForRead | kXMPFiles_OpenUseSmartHandler;
 
-		bool ok;
-		SXMPFiles myFile;
-		std::string status = "";
+        bool ok;
+        SXMPFiles myFile;
+        std::string status = "";
 
-		// First we try and open the file
-		ok = myFile.OpenFile(full_file_path, kXMP_UnknownFile, opts);
-		if (!ok)
-		{
-			status += "No smart handler available for " + full_file_path + "\n";
-			status += "Trying packet scanning.\n";
+        // First we try and open the file
+        ok = myFile.OpenFile(full_file_path, kXMP_UnknownFile, opts);
+        if (!ok) {
+            status += "No smart handler available for " + full_file_path + "\n";
+            status += "Trying packet scanning.\n";
 
-			// Now try using packet scanning
-			opts = kXMPFiles_OpenForUpdate | kXMPFiles_OpenUsePacketScanning;
-			ok = myFile.OpenFile(full_file_path, kXMP_UnknownFile, opts);
-		}
+            // Now try using packet scanning
+            opts = kXMPFiles_OpenForUpdate | kXMPFiles_OpenUsePacketScanning;
+            ok = myFile.OpenFile(full_file_path, kXMP_UnknownFile, opts);
+        }
 
-		// If the file is open then read the metadata
-		if (ok)
-		{
-			// Create the xmp object and get the xmp data
-			SXMPMeta meta;
-			myFile.GetXMP(&meta);
+        // If the file is open then read the metadata
+        if (ok) {
+            // Create the xmp object and get the xmp data
+            SXMPMeta meta;
+            myFile.GetXMP(&meta);
 
-			// Get the the entire dc:subject array
-			int n_tags = meta.CountArrayItems(kXMP_NS_DC, "subject");
-			for (int i = 1; i <= n_tags; i++)
-			{
-				string tag;
-				meta.GetArrayItem(kXMP_NS_DC, "subject", i, &tag, 0);
-				tags.push_back(tag);
-			}
+            // Get the the entire dc:subject array
+            int n_tags = meta.CountArrayItems(kXMP_NS_DC, "subject");
+            for (int i = 1; i <= n_tags; i++) {
+                string tag;
+                meta.GetArrayItem(kXMP_NS_DC, "subject", i, &tag, 0);
+                tags.push_back(tag);
+            }
 
-			// Close the SXMPFile.  The resource file is already closed if it was
-			// opened as read only but this call must still be made.
-			myFile.CloseFile();
-		}
-		else
-		{
-			cout << "Unable to open " << full_file_path << endl;
-		}
-	}
-	catch (XMP_Error &e)
-	{
-		cout << "ERROR: " << e.GetErrMsg() << endl;
-	}
+            // Close the SXMPFile.  The resource file is already closed if it was
+            // opened as read only but this call must still be made.
+            myFile.CloseFile();
+        } else {
+            cout << "Unable to open " << full_file_path << endl;
+        }
+    } catch (XMP_Error &e) {
+        cout << "ERROR: " << e.GetErrMsg() << endl;
+    }
 
-	// Terminate the toolkit
-	SXMPFiles::Terminate();
-	SXMPMeta::Terminate();
+    // Terminate the toolkit
+    SXMPFiles::Terminate();
+    SXMPMeta::Terminate();
 
-	return tags;
+    return tags;
 }
